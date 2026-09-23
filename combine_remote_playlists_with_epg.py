3c12aa076ac9f9a5888cfc91391064edae5d549b
2027iptv2027iptv2027 <2027.iptv2027.iptv2027@gmail.com>
2026-09-22 23:26:56 -0500

    Update combine_remote_playlists_with_epg.py

--- combine_remote_playlists_with_epg.py
+++ combine_remote_playlists_with_epg.py
@@ -7,6 +7,7 @@ import re
 PLAYLISTS = [
     "https://github.com/2027iptv2027iptv2027/demon_m3/raw/refs/heads/main/playlists/m3u/300rip-wo-events.m3u8", 
     "https://github.com/2027iptv2027iptv2027/demon_m3/raw/refs/heads/main/playlists/m3u/dancetv.m3u8", 
+    "https://github.com/2027iptv2027iptv2027/demon_m3/raw/refs/heads/main/playlists/m3u/iheart.m3u8", 
     "https://github.com/2027iptv2027iptv2027/demon_m3/raw/refs/heads/main/playlists/m3u/direct.m3u8", 
     "https://github.com/2027iptv2027iptv2027/demon_m3/raw/refs/heads/main/playlists/m3u/plutotv_gb.m3u", 
     "https://github.com/2027iptv2027iptv2027/demon_m3/raw/refs/heads/main/playlists/m3u/plutotv_us.m3u", 
